59ef0ca721c00819d4598c027512b9532e6a3065
Natan <natan.morote@gmail.com>
2021-02-17 00:59:08 +0100

    Added logout to home screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,10 +2,19 @@
 
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { actions as userActions } from '../ducks/user';
 import Button from '../components/Button';
 import Background from '../components/Background';
 
 export default function Home({ navigation }) {
+  const user = useSelector(state => state.user.data);
+  const dispatch = useDispatch();
+
+  function signOut() {
+    dispatch(userActions.logout(user));
+  };
+
   return (
     <Background>
       <Button mode="outlined" onPress={() => navigation.navigate('Login')}>
@@ -16,6 +25,9 @@ export default function Home({ navigation }) {
       </Button>
       <Button mode="outlined" onPress={() => navigation.navigate('Dashboard')}>
         Dashboard
+      </Button>
+      <Button mode="outlined" onPress={() => signOut()}>
+        Logout
       </Button>
     </Background>
   );
