90af5cfef4e36a7e4e5a0729b3e85e51ccdaba0a
Natan <natan.morote@gmail.com>
2021-06-02 04:11:33 +0200

    helper check end of scroll view

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -3,6 +3,7 @@
 import store from '../store';
 import { actions as alertActions } from '../ducks/alert';
 import { commaToPeriod } from './helpers';
+import { theme } from '../theme';
 
 export function handleAlert(err){
   let message;
@@ -10,4 +11,10 @@ export function handleAlert(err){
       ? message = err.response.data.message
       : message = 'Server connection error'
   store.dispatch(alertActions.error(message));
+}
+
+// Check if ScrollView has been scrolled till the end
+export function isEndOfScroll(nativeEvent){
+	const { layoutMeasurement, contentOffset, contentSize } = nativeEvent;
+	return layoutMeasurement.height + contentOffset.y >= contentSize.height - theme.container.padding;
 }
