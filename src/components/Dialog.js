2269c05bc227002c7b7f42fe0a378599a6b75d2c
baronnath <natan.morote@gmail.com>
2021-12-19 02:11:53 +0100

    Scrollable dialog comp

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -5,15 +5,23 @@ import { Portal, Dialog as PaperDialog } from 'react-native-paper';
 import Button from './Button';
 import { theme } from '../theme';
 
-const Dialog = ({ isVisible, setVisible, title, cancelButton, children, ...props }) => {
+const Dialog = ({ isVisible, setVisible, title, cancelButton, scrollable = false, children, ...props }) => {
 
     return (
       <Portal>
         <PaperDialog visible={isVisible} onDismiss={() => setVisible(false)} theme={theme}>
           <PaperDialog.Title>{title}</PaperDialog.Title>
-          <PaperDialog.Content>
-            { children }
-          </PaperDialog.Content>
+          { scrollable ?
+            <PaperDialog.ScrollArea>
+              <ScrollView contentContainerStyle={{paddingHorizontal: 24}}>
+                { children }
+              </ScrollView>
+            </PaperDialog.ScrollArea>
+            :
+            <PaperDialog.Content>
+              { children }
+            </PaperDialog.Content>
+          }
           <PaperDialog.Actions>
             <Button onPress={() => setVisible(false)} mode="outlined">{cancelButton || 'Cancel'}</Button>
           </PaperDialog.Actions>
