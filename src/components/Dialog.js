066614f0581e438c92ae197240a6611c44539156
baronnath <natan.morote@gmail.com>
2021-12-21 01:52:51 +0100

    - dialog component improved - more action options - style

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,19 +1,25 @@
 // src/components/Dialog.js
 
-import React, { memo, useState } from 'react';
+import React, { memo } from 'react';
+import { useSelector } from 'react-redux';
+import { StyleSheet, ScrollView } from 'react-native';
 import { Portal, Dialog as PaperDialog } from 'react-native-paper';
 import Button from './Button';
 import { theme } from '../theme';
+import translator from '../translator/translator';
 
-const Dialog = ({ isVisible, setVisible, title, cancelButton, scrollable = false, children, ...props }) => {
+const Dialog = ({ isVisible, setVisible, title, actions, scrollable = false, children, ...props }) => {
+
+    const user = useSelector(state => state.user.data);
+    const i18n = translator(user.locale);
 
     return (
       <Portal>
-        <PaperDialog visible={isVisible} onDismiss={() => setVisible(false)} theme={theme}>
+        <PaperDialog visible={isVisible} onDismiss={() => setVisible(false)} theme={theme} style={styles.container}>
           <PaperDialog.Title>{title}</PaperDialog.Title>
           { scrollable ?
             <PaperDialog.ScrollArea>
-              <ScrollView contentContainerStyle={{paddingHorizontal: 24}}>
+              <ScrollView contentContainerStyle={styles.scroll}>
                 { children }
               </ScrollView>
             </PaperDialog.ScrollArea>
@@ -23,7 +29,11 @@ const Dialog = ({ isVisible, setVisible, title, cancelButton, scrollable = false
             </PaperDialog.Content>
           }
           <PaperDialog.Actions>
-            <Button onPress={() => setVisible(false)} mode="outlined">{cancelButton || 'Cancel'}</Button>
+            { actions ?
+                actions
+                :
+                <Button onPress={() => setVisible(false)} mode="outlined">{i18n.t('general.cancel')}</Button>
+            }
           </PaperDialog.Actions>
         </PaperDialog>
       </Portal>
@@ -31,3 +41,13 @@ const Dialog = ({ isVisible, setVisible, title, cancelButton, scrollable = false
 }
 
 export default memo(Dialog);
+
+const styles = StyleSheet.create({
+  container: {
+    maxHeight: '80%',
+  },
+  scroll: {
+    paddingVertical: 10,
+    // maxHeight: '80%',
+  },
+});
