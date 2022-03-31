8d92abfe25b9f81f9516dfb5dfddd10c24d07320
Natan <natan.morote@gmail.com>
2022-03-31 16:16:08 +0200

    beta theme added
    - new beta theme added
    - real theme restyled

--- src/theme.js
+++ src/theme.js
@@ -3,23 +3,56 @@
 import { DarkTheme } from 'react-native-paper';
 import color from 'color';
 
+// // Betta colors (green)
+// export const theme = {
+//   ...DarkTheme,
+//   colors: {
+//     ...DarkTheme.colors,
+//     primary: '#7ECA9C',
+//    	accent: '#CCFFBD',
+//     secondary: '#CCFFBD',
+//     // terciary: '#04decf',
+//     // quaternary: '#04aad4',
+//     // error: '#fd063c',
+//     warning: '#ff9300',
+//     // notification: '#ff9300',
+//     text: '#ffffff',
+//     lightText: '#969696',
+//     // background: '#f6f6f6',
+//     // placeholder: color('#000000').alpha(0.54).rgb().string(),
+//     disabled: color('#ffffff').alpha(0.26).rgb().string(),
+//     // paper: '#000000',
+//     transparent: color('#000000').alpha(0).rgb().string(),
+//   },
+//   container: {
+//     maxWidth: 400,
+//     padding: 20,
+//   },
+//   bottomNav: {
+//     height: 50,
+//   },
+// };
+
+
 export const theme = {
   ...DarkTheme,
   colors: {
     ...DarkTheme.colors,
-    primary: '#00bcba',
-   	accent: '#ff9300',
-    secondary: '#ff9300',
-    // terciary: '#04decf',
-    // quaternary: '#04aad4',
-    // error: '#fd063c',
-    // notification: '#ff9300',
-    text: '#ffffff',
+    primary: '#45a29e',
+    accent: '#66fcf1',
+    secondary: '#66fcf1',
+    terciary: '#c5c6c7',
+    // quaternary: '#ffc045',
+    error: '#fc6b3f',
+    warning: '#ec9b3b',
+    notification: '#ff9300',
+    text: '#c5c6c7',
     lightText: '#969696',
-    // background: '#f6f6f6',
-    // placeholder: color('#000000').alpha(0.54).rgb().string(),
-    disabled: color('#ffffff').alpha(0.26).rgb().string(),
-    // paper: '#000000',
+    background: '#000000',
+    surface: '#0f1419',
+    // onSurface: '#FFFFFF',
+    placeholder: color('#c5c6c7').alpha(0.54).rgb().string(),
+    disabled: color('#c5c6c7').alpha(0.26).rgb().string(),
     transparent: color('#000000').alpha(0).rgb().string(),
   },
   container: {
