6cd3cce56851b3b15eca9edc53a75bf6136986a6
Natan <natan.morote@gmail.com>
2023-01-28 02:12:43 +0100

    button impoved
    - Added disabled style
    - Added compact style
    - Added custom label style

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,26 +4,48 @@ import React, { memo } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button as PaperButton } from 'react-native-paper';
 import { theme } from '../theme';
+import color from 'color';
 
-const Button = ({ mode, style, icon, children, ...props }) => {
+const Button = ({ mode, style, labelStyle, icon, disabled = false, compact = false, children, ...props }) => {
   let surface = false;
   if(mode === 'outlined' || mode === 'text')
     surface = true;
+
+  let backgroundColor = theme.colors.primary;
+  if (surface) 
+    backgroundColor = theme.colors.paper;
+
+  const styles = StyleSheet.create({
+    button: {
+      width: '100%',
+      marginVertical: 10,
+      backgroundColor: disabled ? color(backgroundColor).alpha(0.12).rgb().string() : backgroundColor,
+      justifyContent: 'center',
+    },
+    label: {
+      color: theme.colors.surface,
+      fontWeight: 'bold',
+      fontSize: 15,
+      lineHeight: 26,
+    },
+  });
 
   return(
     <PaperButton
       style={[
         styles.button,
-        surface && { backgroundColor: theme.colors.paper },
         style,
       ]}
       labelStyle={[
         styles.label,
         surface && { color: theme.colors.primary },
+        labelStyle
       ]}
       mode={mode}
       icon={icon} 
       theme={theme}
+      // disabled={disabled}
+      compact={compact}
       {...props}
     >
       {children}
@@ -31,19 +53,6 @@ const Button = ({ mode, style, icon, children, ...props }) => {
   )
 };
 
-const styles = StyleSheet.create({
-  button: {
-    width: '100%',
-    marginVertical: 10,
-    backgroundColor: theme.colors.primary,
-    justifyContent: 'center',
-  },
-  label: {
-    color: theme.colors.surface,
-    fontWeight: 'bold',
-    fontSize: 15,
-    lineHeight: 26,
-  },
-});
+
 
 export default memo(Button);
